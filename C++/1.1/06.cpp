64acaca962af1059db20637939182323b21451b8
Jesse Guo <66837755+JesseGuoTechnology@users.noreply.github.com>
2021-03-10 21:33:09 +0800

    Create 06.cpp

---
+++ C++/1.1/06.cpp
@@ -0,0 +1,13 @@
+#include<cstdio>
+#include<iostream>
+using namespace std;
+main(){
+	char a;
+	int b;
+	float c;
+	double d;
+	cin>>a>>b>>c>>d;
+	printf("%c %d %.6lf %.6lf",a,b,c,d);
+	system("pause");
+	return EXIT_SUCCESS;
+}
