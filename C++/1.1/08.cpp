d2b94ed0d4b8593170cea2d23d96693124be8eb8
Jesse Guo <66837755+JesseGuoTechnology@users.noreply.github.com>
2021-03-10 21:33:26 +0800

    Create 08.cpp

---
+++ C++/1.1/08.cpp
@@ -0,0 +1,10 @@
+#include<cstdio>
+#include<iostream>
+using namespace std;
+main(){
+	char a;
+	cin>>a;
+	printf("  %c\n %c%c%c\n%c%c%c%c%c\n",a,a,a,a,a,a,a,a,a);
+	system("pause");
+	return EXIT_SUCCESS;
+}
